e97da6d20c8eca8914739cbef779c054a6f53362
Piyush Varma <pytworks@gmail.com>
2026-01-31 18:39:28 +0530

    functions

---
+++ functions1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printName(string name){
+    cout << "Hello " << name << endl;
+}
+
+int main(){
+    string chutiya;
+    cout << "Enter the name of chutiya: ";
+    cin >> chutiya;
+
+    printName(chutiya);
+    return 0;
+}
