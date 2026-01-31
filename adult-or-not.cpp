2c7c36bd4e93415966923de2c3173a5825d96b95
Piyush Varma <pytworks@gmail.com>
2026-01-31 16:31:45 +0530

    adult or not

---
+++ adult-or-not.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Whats your age?" << endl;
+    cin >> age;
+
+    if (age>=18){
+        cout << "you are an adult babyboi" << endl;
+    } 
+    else {
+        cout<< "you are not an adult babyboi" << endl;
+    }
+    return 0;
+}
